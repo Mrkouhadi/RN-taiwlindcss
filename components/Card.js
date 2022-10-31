b1ae7ec412908312e3a7c7818ef929c6297c9d61
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-10-31 20:40:36 +0800

    added card

---
+++ components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Card = () => {
+  return (
+    <View>
+        <Text className="bg-blue-400"> Hello Card</Text>
+    </View>
+  )
+}
+
+export default Card
