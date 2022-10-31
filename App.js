0b01dd98c45ee5f30ef4efe2fc793d75fe879035
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-10-31 22:35:32 +0800

    added dark mode using tailwindcss

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Switch,Text, View } from 'react-native';
+import Card from './components/Card';
+import { useColorScheme } from 'nativewind'
+
 
 export default function App() {
+  const {colorScheme,toggleColorScheme } = useColorScheme()
+
   return (
-    <View className="flex-1 p-8 bg-yellow-400 items-center justify-start ">
-        <Text className="text-3xl text-center">Open up App.js to start working on your app!</Text>
+    <View className="flex-1 p-4 bg-blue-500 dark:bg-black items-center justify-center ">
       <StatusBar style="auto" />
+      <Switch value={colorScheme === 'light'} onChange={toggleColorScheme} />
+        <Card/>
+        <Text className="text-yellow-200 text-3xl text-center">Open up App.js to start working on your app!</Text>
     </View>
   )
 }
