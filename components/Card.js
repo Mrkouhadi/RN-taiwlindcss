1dbc34cb33dc6da2e0956b434f89491ce4b5aa82
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-11-01 03:35:20 +0800

    wrapped up the project

--- components/Card.js
+++ components/Card.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Image, Text, View } from 'react-native'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
 import { useColorScheme } from 'nativewind'
 import {AntDesign} from '@expo/vector-icons'
 
@@ -7,10 +7,9 @@ const Card = ({title,description,price, image,category}) => {
     const [count, setCount] = useState(1)
     const {colorScheme} = useColorScheme()
 
-    
   return (
-    <View className="w-full bg-white dark:bg-gray-50/10 rounded-3xl my-2">
-        <View className="bg-white rounded-xl">
+    <View className="w-full bg-white dark:bg-gray-50/10 rounded-3xl pb-2 my-2">
+        <View className="bg-white rounded-2xl">
             <Image source={{uri:image}} className="w-full h-72" style={{resizeMode:'contain'}}/>
         </View>
         <View className="mt-3 p-2">
@@ -29,9 +28,12 @@ const Card = ({title,description,price, image,category}) => {
                         onPress={()=> setCount(count+1)}
                 />
             </View>
-            <Text className="text-2xl font-extrabold dark:text-white" >${price * count}</Text>
+            <Text className="text-2xl font-extrabold dark:text-white" >${(price * count).toFixed(2)}</Text>
         </View>
             <Text className="p-2 text-sm text-black/60 dark:text-white/70" numberOfLines={2} >{description}</Text>
+            <TouchableOpacity className="bg-black dark:bg-white h-12 mx-2 rounded-2xl items-center justify-center ">
+                <Text className="text-white dark:text-black text-xl">Add to Cart</Text>
+            </TouchableOpacity>
     </View>
   )
 }
--- components/ProductsList.js
+++ components/ProductsList.js
@@ -1,16 +1,24 @@
 import React from 'react'
-import { FlatList, Text } from 'react-native'
+import { FlatList } from 'react-native'
 import products from '../dummy-data'
 import Card from './Card'
+import {Dimensions} from 'react-native';
 
 const ProductsList = () => {
+  const DeviceWidth = Dimensions.get('window').width
+  const FilterNumCols = ()=>{
+    if(DeviceWidth >= 1200  ) return 3
+    if(DeviceWidth >= 800  ) return 2
+    if(DeviceWidth < 800) return 1
+  }
 
  return (
     <FlatList
+        numColumns={FilterNumCols()}
         data={products}
         keyExtractor={prod => prod.id}
-        renderItem={({item}) =><Card {...item}/>}
-        className="px-2"
+        renderItem={({item}) => <Card {...item}/>}
+        className="px-2 w-full"
     />
   )
 }
