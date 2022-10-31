16d761853bde7e269eebc94fa6d88e0882c3de32
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-10-31 22:36:40 +0800

    added dark

--- components/Card.js
+++ components/Card.js
@@ -3,8 +3,8 @@ import { Text, View } from 'react-native'
 
 const Card = () => {
   return (
-    <View>
-        <Text className="bg-blue-400"> Hello Card</Text>
+    <View className="h-32 border-8 border-red-400 rounded-full items-center justify-center bg-blue-400 w-screen">
+        <Text className=""> Hello Card</Text>
     </View>
   )
 }
